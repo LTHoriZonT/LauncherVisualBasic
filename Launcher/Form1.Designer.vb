eaa1c8c66027f5cf0e7b23767ad99b5dabb254a8
HoriZonT <isaquemendes405@gmail.com>
2021-02-01 14:59:06 -0300

    Upload do código finalizado

---
+++ Launcher/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: O procedimento a seguir é exigido pelo Windows Form Designer
+    'Ele pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowNavigation = False
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 5)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(471, 328)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.TabStop = False
+        Me.WebBrowser1.Url = New System.Uri("http://progame22.wix.com/launcher", System.UriKind.Absolute)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.SystemColors.Control
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.Crimson
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 339)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(259, 40)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.ForeColor = System.Drawing.Color.Crimson
+        Me.Button1.Location = New System.Drawing.Point(380, 339)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 59)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Sair"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button2.Location = New System.Drawing.Point(277, 339)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 27)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Baixar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Location = New System.Drawing.Point(278, 372)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 25)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Instalar "
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(13, 385)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(258, 13)
+        Me.ProgressBar2.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "V1.2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(479, 403)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Cursor = System.Windows.Forms.Cursors.No
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WarCraft Launcher (V1.2)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Launcher/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.IO.Compression
+Public Class Form1
+    WithEvents Webclient1 As New WebClient
+    WithEvents Webclient2 As New WebClient
+    Dim versaoatual As String = "1.0.0.0"
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim versaodosite As String = Webclient1.DownloadString("https://dl.dropboxusercontent.com/s/fuhca414wikfgis/versao.txt?dl=1&token_hash=AAEh6crMalj09q8FJD8UDtWfBwjV8bxxP7KOhYYDFiVkEQ")
+        If versaodosite > versaoatual Then
+            MessageBox.Show("Atualização Disponivel   >>>...113MB...<<< ")
+            Webclient2.DownloadFileAsync(New Uri("https://dl.dropboxusercontent.com/s/sr5fp3nqe8nb9gp/att.zip?dl=1&token_hash=AAF-kNW2L9Dh7faAa4kvAVbWfvJG4X4iLSmbBqhvc4TdAA"), "C:\Program Files\att.zip")
+            MessageBox.Show("Launcher Feito por ANGEL_GM & Vandro")
+        End If
+    End Sub
+
+    Private Sub Webclient2_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs) Handles Webclient2.DownloadProgressChanged
+        ProgressBar1.Maximum = e.TotalBytesToReceive
+        ProgressBar1.Value = e.BytesReceived
+    End Sub
+    Private Sub Extrair()
+        If MessageBox.Show("Instalando.. Espere Alguns Segundos") Then
+
+        End If
+        Dim Username As String = Environment.UserName
+        Dim AppData As String = "C:\Users\" & Username & "\AppData\Roaming"
+        Dim AD As String = AppData & "\.minecraft"
+
+        Dim sc As New Shell32.Shell
+        IO.Directory.CreateDirectory(AD)
+        Dim _pasta As Shell32.Folder = sc.NameSpace(AD)
+        Dim _arquivo As Shell32.Folder = sc.NameSpace("C:\Program Files\att.zip")
+        _pasta.CopyHere(_arquivo.Items, 4)
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Extrair()
+        If MessageBox.Show("Instalado.. Ja pode Jogar!!") Then
+
+        End If
+    End Sub
+    Private Sub ProgressBar2_Click(sender As Object, e As EventArgs) Handles ProgressBar2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs)
+
+    End Sub
+End Class
